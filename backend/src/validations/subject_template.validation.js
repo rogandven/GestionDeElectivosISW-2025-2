1aaa21c974ce38c7db8a2079f53e59d64e342eb5
Roger Venegas <roger.venegas2301@alumnos.ubiobio.cl>
2025-11-07 20:48:12 -0300

    Added subject template validation

---
+++ backend/src/validations/subject_template.validation.js
@@ -0,0 +1,30 @@
+"use strict";
+import Joi from "joi";
+import { MIN_NAME_LENGTH, MAX_NAME_LENGTH, MIN_DESC_LENGTH, MAX_DESC_LENGTH, MIN_SPOTS, MAX_SPOTS, MIN_CREDITS, MAX_CREDITS } from "./constants/validationConstants.js";
+import { validateTimeStamp } from "./modules/timestamp.validation.js";
+
+export const subjectTemplateIntegrityValidation = Joi.object({
+    name: Joi.string().min(MIN_NAME_LENGTH).max(MAX_NAME_LENGTH),
+    description: Joi.string().min(MIN_DESC_LENGTH).max(MAX_DESC_LENGTH),
+    spots_per_career: Joi.number().integer().min(MIN_SPOTS).max(MAX_SPOTS),
+    required_credits: Joi.number().integer().min(MIN_CREDITS).max(MAX_CREDITS),
+    minimum_semester: Joi.string().custom(validateTimeStamp),
+});
+
+export const subjectTemplateCreationValidation = Joi.object({
+    name: Joi.any().required(),
+    description: Joi.any().required(),
+    spots_per_career: Joi.any().required(),
+    required_credits: Joi.any().required(),
+    minimum_semester: Joi.any().required(),
+}).unknown(false);
+
+export const subjectTemplateEditingValidation = Joi.object({
+    name: Joi.any(),
+    description: Joi.any(),
+    spots_per_career: Joi.any(),
+    required_credits: Joi.any(),
+    minimum_semester: Joi.any(),
+}).unknown(false);
+
+export default subjectTemplateIntegrityValidation;
