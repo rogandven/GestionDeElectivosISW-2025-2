ffa36667a361acfebb48d60e251be382624bc368
Roger Venegas <roger.venegas2301@alumnos.ubiobio.cl>
2025-11-08 05:24:51 -0300

    tet

--- backend/src/controllers/career.controller.js
+++ backend/src/controllers/career.controller.js
@@ -1,18 +1,26 @@
 import { AppDataSource } from "../config/configDb.js";
 import Career from "../entity/career.entity.js";
 
+const RELATIONS = ["subject", "user"];
+
 export async function getCareers(req, res) {
     try {
         const careerRepository = AppDataSource.getRepository(Career);
+        const careers = await asambleaRepository.find({
+            relations: RELATIONS,
+        });
+        return res.status(200).json({ message: "Carreras encontradas", data: careers });
     } catch (error) {
-        console.error("Error al obtener las careers", error);
-        res.status(500).json({ message: "Error al obtener las careers" });
+        console.error("Error al obtener las carreras", error);
+        res.status(500).json({ message: "Error al obtener las carreras" });
     }
 }
 
 export async function createCareer(req, res) {
     try {
         const careerRepository = AppDataSource.getRepository(Career);
+        
+
     } catch (error) {
         console.error("Error al crear career", error);
         res.status(500).json({ message: "Error al crear career." });
