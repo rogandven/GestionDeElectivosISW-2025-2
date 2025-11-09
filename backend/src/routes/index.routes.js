9adfd8d7cf59ac0625dc3da222759b5500ace26e
Roger Venegas <roger.venegas2301@alumnos.ubiobio.cl>
2025-11-09 15:56:10 -0300

    Initialized routes

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -2,10 +2,25 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js"
 import userRoutes from "./user.routes.js";
+import careerRoutes from "./career.routes.js";
+import inscriptionRoutes from "./inscription.routes.js";
+import preinscriptionRoutes from "./preinscription.routes.js";
+import scheduleRoutes from "./schedule.routes.js";
+import subjectTemplateRoutes from "./subject_template.routes.js";
+import subjectRoutes from "./subject.routes.js";
+
+
+
 
 const router = new Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.use("/careers", careerRoutes);
+router.use("/inscriptions", inscriptionRoutes);
+router.use("/preinscriptions", preinscriptionRoutes);
+router.use("/schedules", scheduleRoutes);
+router.use("/subject_templates", subjectTemplateRoutes);
+router.use("/subjects", subjectRoutes);
 
 export default router;
