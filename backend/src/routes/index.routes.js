db977d9763c40a39d8238860174d79a725e72202
Fermin <millanaof38@gmail.com>
2025-11-09 05:11:31 -0300

    aqui esta mi parte :D si hay problema me habla i am sorry por enviar tarde mi parte ademas aqui implemente lo de crear inscripcion, eliminar inscripcion, y las funciones de admin que son obtener las inscripciones y demas

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -4,11 +4,15 @@ import authRoutes from "./auth.routes.js"
 import userRoutes from "./user.routes.js";
 import claseRoutes from "./clase.routes.js";
 
+import nowRoutes from "./now.routes.js";
+import inscripcionRoutes from "./inscripcion.routes.js"
 const router = new Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/clases", claseRoutes);
+router.use("/electivos", nowRoutes);
+router.use("/Inscripciones",inscripcionRoutes);
 
 
 export default router;
